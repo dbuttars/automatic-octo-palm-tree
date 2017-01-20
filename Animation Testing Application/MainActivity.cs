ef66b378b01ff553ee2e2383718fd10b7fbc46d7
Daniel Buttars <dbuttars@asu.edu>
2017-01-20 15:34:51 -0700

    Add project files.

---
+++ Animation Testing Application/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Animation_Testing_Application
+{
+    [Activity(Label = "Animation_Testing_Application", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
