df03a42ed40bcb1156e96620b085c0c9f86ed0a1
Daniel Buttars <dbuttars@asu.edu>
2017-01-20 17:53:37 -0700

    Initial setup and Sample Earth Animation Completed

--- Animation Testing Application/MainActivity.cs
+++ Animation Testing Application/MainActivity.cs
@@ -4,15 +4,30 @@ using Android.OS;
 
 namespace Animation_Testing_Application
 {
+    
     [Activity(Label = "Animation_Testing_Application", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        Android.Graphics.Drawables.AnimationDrawable _earthDrawable;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+            
+
+
+        }
+
+        public override void OnWindowFocusChanged(bool boolean)
+        {
+            _earthDrawable = (Android.Graphics.Drawables.AnimationDrawable)
+                Resources.GetDrawable(Resource.Drawable.earth_animation);
+            ImageView earthImage = FindViewById<ImageView>(Resource.Id.imageView1);
+            earthImage.SetImageDrawable((Android.Graphics.Drawables.Drawable) _earthDrawable);
+            _earthDrawable.Start();
         }
     }
 }
--- Animation Testing Application/Resources/Resource.Designer.cs
+++ Animation Testing Application/Resources/Resource.Designer.cs
@@ -45,7 +45,253 @@ namespace Animation_Testing_Application
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int earth_animation = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int earth_animation_01 = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int earth_animation_02 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int earth_animation_03 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int earth_animation_04 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int earth_animation_05 = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int earth_animation_06 = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int earth_animation_07 = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int earth_animation_08 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int earth_animation_09 = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int earth_animation_10 = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int earth_animation_11 = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int earth_animation_12 = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int earth_animation_13 = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int earth_animation_14 = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int earth_animation_15 = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int earth_animation_16 = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int earth_animation_17 = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int earth_animation_18 = 2130837522;
+			
+			// aapt resource value: 0x7f020013
+			public const int earth_animation_19 = 2130837523;
+			
+			// aapt resource value: 0x7f020014
+			public const int earth_animation_20 = 2130837524;
+			
+			// aapt resource value: 0x7f020015
+			public const int earth_animation_21 = 2130837525;
+			
+			// aapt resource value: 0x7f020016
+			public const int earth_animation_22 = 2130837526;
+			
+			// aapt resource value: 0x7f020017
+			public const int earth_animation_23 = 2130837527;
+			
+			// aapt resource value: 0x7f020018
+			public const int earth_animation_24 = 2130837528;
+			
+			// aapt resource value: 0x7f020019
+			public const int earth_animation_25 = 2130837529;
+			
+			// aapt resource value: 0x7f02001a
+			public const int earth_animation_26 = 2130837530;
+			
+			// aapt resource value: 0x7f02001b
+			public const int earth_animation_27 = 2130837531;
+			
+			// aapt resource value: 0x7f02001c
+			public const int earth_animation_28 = 2130837532;
+			
+			// aapt resource value: 0x7f02001d
+			public const int earth_animation_29 = 2130837533;
+			
+			// aapt resource value: 0x7f02001e
+			public const int earth_animation_30 = 2130837534;
+			
+			// aapt resource value: 0x7f02001f
+			public const int earth_animation_31 = 2130837535;
+			
+			// aapt resource value: 0x7f020020
+			public const int earth_animation_32 = 2130837536;
+			
+			// aapt resource value: 0x7f020021
+			public const int earth_animation_33 = 2130837537;
+			
+			// aapt resource value: 0x7f020022
+			public const int earth_animation_34 = 2130837538;
+			
+			// aapt resource value: 0x7f020023
+			public const int earth_animation_35 = 2130837539;
+			
+			// aapt resource value: 0x7f020024
+			public const int earth_animation_36 = 2130837540;
+			
+			// aapt resource value: 0x7f020025
+			public const int earth_animation_37 = 2130837541;
+			
+			// aapt resource value: 0x7f020026
+			public const int earth_animation_38 = 2130837542;
+			
+			// aapt resource value: 0x7f020027
+			public const int earth_animation_39 = 2130837543;
+			
+			// aapt resource value: 0x7f020028
+			public const int earth_animation_40 = 2130837544;
+			
+			// aapt resource value: 0x7f020029
+			public const int earth_animation_41 = 2130837545;
+			
+			// aapt resource value: 0x7f02002a
+			public const int earth_animation_42 = 2130837546;
+			
+			// aapt resource value: 0x7f02002b
+			public const int earth_animation_43 = 2130837547;
+			
+			// aapt resource value: 0x7f02002c
+			public const int earth_animation_44 = 2130837548;
+			
+			// aapt resource value: 0x7f02002d
+			public const int earth_animation_45 = 2130837549;
+			
+			// aapt resource value: 0x7f02002e
+			public const int earth_animation_46 = 2130837550;
+			
+			// aapt resource value: 0x7f02002f
+			public const int earth_animation_47 = 2130837551;
+			
+			// aapt resource value: 0x7f020030
+			public const int earth_animation_48 = 2130837552;
+			
+			// aapt resource value: 0x7f020031
+			public const int earth_animation_49 = 2130837553;
+			
+			// aapt resource value: 0x7f020032
+			public const int earth_animation_50 = 2130837554;
+			
+			// aapt resource value: 0x7f020033
+			public const int earth_animation_51 = 2130837555;
+			
+			// aapt resource value: 0x7f020034
+			public const int earth_animation_52 = 2130837556;
+			
+			// aapt resource value: 0x7f020035
+			public const int earth_animation_53 = 2130837557;
+			
+			// aapt resource value: 0x7f020036
+			public const int earth_animation_54 = 2130837558;
+			
+			// aapt resource value: 0x7f020037
+			public const int earth_animation_55 = 2130837559;
+			
+			// aapt resource value: 0x7f020038
+			public const int earth_animation_56 = 2130837560;
+			
+			// aapt resource value: 0x7f020039
+			public const int earth_animation_57 = 2130837561;
+			
+			// aapt resource value: 0x7f02003a
+			public const int earth_animation_58 = 2130837562;
+			
+			// aapt resource value: 0x7f02003b
+			public const int earth_animation_59 = 2130837563;
+			
+			// aapt resource value: 0x7f02003c
+			public const int earth_animation_60 = 2130837564;
+			
+			// aapt resource value: 0x7f02003d
+			public const int earth_animation_61 = 2130837565;
+			
+			// aapt resource value: 0x7f02003e
+			public const int earth_animation_62 = 2130837566;
+			
+			// aapt resource value: 0x7f02003f
+			public const int earth_animation_63 = 2130837567;
+			
+			// aapt resource value: 0x7f020040
+			public const int earth_animation_64 = 2130837568;
+			
+			// aapt resource value: 0x7f020041
+			public const int earth_animation_65 = 2130837569;
+			
+			// aapt resource value: 0x7f020042
+			public const int earth_animation_66 = 2130837570;
+			
+			// aapt resource value: 0x7f020043
+			public const int earth_animation_67 = 2130837571;
+			
+			// aapt resource value: 0x7f020044
+			public const int earth_animation_68 = 2130837572;
+			
+			// aapt resource value: 0x7f020045
+			public const int earth_animation_69 = 2130837573;
+			
+			// aapt resource value: 0x7f020046
+			public const int earth_animation_70 = 2130837574;
+			
+			// aapt resource value: 0x7f020047
+			public const int earth_animation_71 = 2130837575;
+			
+			// aapt resource value: 0x7f020048
+			public const int earth_animation_72 = 2130837576;
+			
+			// aapt resource value: 0x7f020049
+			public const int earth_animation_73 = 2130837577;
+			
+			// aapt resource value: 0x7f02004a
+			public const int earth_animation_74 = 2130837578;
+			
+			// aapt resource value: 0x7f02004b
+			public const int earth_animation_75 = 2130837579;
+			
+			// aapt resource value: 0x7f02004c
+			public const int earth_animation_76 = 2130837580;
+			
+			// aapt resource value: 0x7f02004d
+			public const int earth_animation_77 = 2130837581;
+			
+			// aapt resource value: 0x7f02004e
+			public const int earth_animation_78 = 2130837582;
+			
+			// aapt resource value: 0x7f02004f
+			public const int earth_animation_79 = 2130837583;
+			
+			// aapt resource value: 0x7f020050
+			public const int earth_animation_80 = 2130837584;
+			
+			// aapt resource value: 0x7f020051
+			public const int earth_half = 2130837585;
+			
+			// aapt resource value: 0x7f020052
+			public const int Icon = 2130837586;
 			
 			static Drawable()
 			{
@@ -53,6 +299,22 @@ namespace Animation_Testing_Application
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int imageView1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
